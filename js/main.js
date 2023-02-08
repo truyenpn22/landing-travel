79b6410fe592e91745c24ea8c8cdea83040d4fc3
truyenpn22 <phantruyen19722@gmail.com>
2023-02-08 21:26:13 +0700

    up new

--- js/main.js
+++ js/main.js
@@ -22,6 +22,19 @@ $(document).ready(function(){
         }
     });
 
+     // Back to top button
+     $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body, #').animate({scrollTop: 0}, 1000, 'easeInOutExpo');
+        return false;
+    });
+
     //loading JS
     
     setTimeout(function() {
